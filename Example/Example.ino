ec31d3c4f70b2a420ea4d91378be300a47842eec
Mohammed <MD.2015.1@outlook.com>
2020-09-26 18:11:41 +0330

    Create Example.ino

---
+++ Example/Example.ino
@@ -0,0 +1,60 @@
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+
+/*
+   This sample demonstrates TinyGPS++'s capacity for extracting custom
+   fields from any NMEA sentence.  TinyGPS++ has built-in facilities for
+   extracting latitude, longitude, altitude, etc., from the $GPGLL and
+   $GPRMC sentences.  But with the TinyGPSCustom type, you can extract
+   other NMEA fields, even from non-standard NMEA sentences.
+
+   It requires the use of SoftwareSerial, and assumes that you have a
+   4800-baud serial GPS device hooked up on pins 4(RX) and 3(TX).
+*/
+static const int RXPin = 4, TXPin = 3;
+static const uint32_t GPSBaud = 9600;
+
+// The TinyGPS++ object
+TinyGPSPlus gps;
+
+// The serial connection to the GPS device
+SoftwareSerial ss(RXPin, TXPin);
+
+/*
+   By declaring TinyGPSCustom objects like this, we announce that we
+   are interested in the 15th, 16th, and 17th fields in the $GPGSA
+   sentence, respectively the PDOP (F("positional dilution of precision")),
+   HDOP (F("horizontal...")), and VDOP (F("vertical...")).
+
+   (Counting starts with the field immediately following the sentence name,
+   i.e. $GPGSA.  For more information on NMEA sentences, consult your
+   GPS module's documentation and/or http://aprs.gids.nl/nmea/.)
+
+   If your GPS module doesn't support the $GPGSA sentence, then you
+   won't get any output from this program.
+*/
+
+TinyGPSCustom pdop(gps, "GNGLL", 1); // $GPGSA sentence, 15th element
+TinyGPSCustom hdop(gps, "GNGLL", 3); // $GPGSA sentence, 16th element
+
+void setup()
+{
+  Serial.begin(115200);
+  ss.begin(GPSBaud);
+
+  Serial.println(F("UsingCustomFields.ino"));
+  Serial.println(F("Demonstrating how to extract any NMEA field using TinyGPSCustom"));
+  Serial.print(F("Testing TinyGPS++ library v. ")); Serial.println(TinyGPSPlus::libraryVersion());
+  Serial.println(F("by Mikal Hart"));
+  Serial.println();
+}
+
+void loop()
+{
+  Serial.print(F(" LAT=")); Serial.print(atof(pdop.value())/100,7);
+  Serial.print(F("\tLON=")); Serial.println(atof(hdop.value())/100,7);
+  delay(100);
+
+  while (ss.available() > 0)
+    gps.encode(ss.read());
+}
